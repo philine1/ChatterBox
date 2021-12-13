46a203bef92301b289aaaba979a9cfc58c4c8c13
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2021-12-13 12:38:56 +0000

    added script

--- server/controller/journal.js
+++ server/controller/journal.js
@@ -1,8 +1,9 @@
-const express = require("express")
+const express = require("express");
+const journalEntries = require("../data");
 const router = express.Router();
 
 router.get("/" , (req,res) => {
-    res.send("hi")
+    res.send(journalEntries)
 })
 
 module.exports = router;
---
+++ server/data.js
@@ -0,0 +1,6 @@
+const journalEntries = [
+    {id:1, author:'mighty coders', message:'Hello world!'}
+
+]
+
+module.exports = journalEntries;
