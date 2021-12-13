9023cfc2a44a8fb2a48ad8b485c4cfe580ea455b
philine1 <phparkeu93@gmail.com>
2021-12-13 12:42:11 +0000

    journal route added

--- server/controller/journal.js
+++ server/controller/journal.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const journalEntries = require("../data");
 const router = express.Router();
-const journalEntries = require("../data")
+const journalData = require("../data")
 
 //all routes starting with "/journal"
 router.get("/" , (req,res) => {
-    res.send(journalEntries)
+    res.send("hi")
+})
+
+router.get("/journal" , (req,res) => {
+    res.send(journalData)
 })
 
 module.exports = router;
