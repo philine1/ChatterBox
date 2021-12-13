a0cfbeb5daf5d003f49bd16f6fb23fcefbf81133
philine1 <phparkeu93@gmail.com>
2021-12-13 12:26:55 +0000

    created mock data

--- server/controller/journal.js
+++ server/controller/journal.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router();
+const journalData = require("../data")
 
+//all routes starting with "/journal"
 router.get("/" , (req,res) => {
-    res.send("hi")
+    res.send(journalData)
 })
 
 module.exports = router;
---
+++ server/data.js
@@ -0,0 +1,8 @@
+const journalEntries = [{
+    id: 1,
+    author: "John Sock",
+    message: "I love apple pie",
+    date: "Tue, 14 Sep 2021 11:55:56 GMT"
+}]
+
+module.exports = journalEntries;
