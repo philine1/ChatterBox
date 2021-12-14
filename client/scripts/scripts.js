0712f870eaabfb8c28f15b6a92b86765acab2e1e
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2021-12-14 18:52:29 +0000

    gif fully added

--- client/scripts/scripts.js
+++ client/scripts/scripts.js
@@ -1,3 +1,5 @@
+
+
 const form = document.querySelector('form');
 const chatsFeed = document.querySelector('.chats');
 
@@ -7,7 +9,7 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
     const message = formData.get("message")
-    const name = formData.get("search") // search targets the gif api
+    const gif = formData.get("search") // search targets the gif api
 
     const chat = {
         message,
@@ -17,3 +19,57 @@ form.addEventListener('submit', (e) => {
     console.log(chat);
     
 });
+
+// function to retrieve an API
+document.addEventListener("DOMContentLoaded", sendApiRequest);
+function sendApiRequest(){
+   
+    
+    document.getElementById("btnSearch").addEventListener("click", e => {
+        e.preventDefault();
+        
+        var apiKey = "74d7MBRbZL0YmhJiyOtbVF20N7j0XfUx"
+        var userInput = document.getElementById("search").value.trim();
+        console.log(userInput)
+        var giphyURL = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=5&q=`
+        giphyURL = giphyURL.concat(userInput)
+        console.log(giphyURL);
+
+        fetch(giphyURL)
+            .then(response => response.json())
+            .then(content => {
+                // DataTransfer, pagination, meta
+                console.log(content.data);
+                console.log("META", content.meta);
+                let fig = document.createElement("figure");
+                let img = document.createElement("img");
+                // let fc = document.createElement("figcaption");
+                img.src = content.data[Math.floor(Math.random() * 5)].images.downsized.url;
+                img.alt = content.data[0].title;
+                // fc.textContent = content.data[0].title;
+                fig.appendChild(img);
+                // fig.appendChild(fc);
+                let out = document.querySelector("#out");
+                out.insertAdjacentElement("afterbegin", fig);
+                document.querySelector("#search").value = "";
+
+
+
+                       
+            })
+            .catch(err =>{
+                console.log(err)
+            })
+        
+        // fetch(giphyURL).then(function(data){
+        //       return data.JSON()
+        //     })
+        //     .then(function(json){
+        //           console.log(json.data[0].images.fixed_height.url) 
+        //           var imgPath = json.data[0].images.fixed_height.url
+        //           var img = document.createElement("img")
+        //           img.setAttribute("src", imgPath)
+        //           document.body.appendChild(img)
+                
+            })
+  };
