981fc52e86ff7f47733fc10d1b482e30a7d960fa
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2021-12-14 14:47:08 +0000

    gif added to html

--- client/scripts/scripts.js
+++ client/scripts/scripts.js
@@ -6,12 +6,12 @@ const chatsFeed = document.querySelector('.chats');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
-    const name = formData.get("name")
     const message = formData.get("message")
+    const name = formData.get("search")
 
     const chat = {
-        name,
-        message
+        message,
+        gif
     };
 
     console.log(chat);
